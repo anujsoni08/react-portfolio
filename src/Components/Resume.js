92eefad97e65d4cf379099dc8a9371ad97c2bbca
anujsoni08 <anujsoni08@gmail.com>
2020-12-17 23:57:55 +0530

    initial commit

---
+++ src/Components/Resume.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const Resume = (props) => {
+  let skillmessage = props.data.skillmessage;
+  let education = props.data.education.map(function (educationObj) {
+    return (
+      <div key={educationObj.school}>
+        <h3>{educationObj.school}</h3>
+        <p className="info">
+          {educationObj.degree} <span>&bull;</span>
+          <em className="date">{educationObj.graduated}</em>
+        </p>
+        <p>{educationObj.description}</p>
+      </div>
+    );
+  });
+  let work = props.data.work.map(function (work) {
+    return (
+      <div key={work.company}>
+        <h3>{work.company}</h3>
+        <p className="info">
+          {work.title}
+          <span>&bull;</span> <em className="date">{work.years}</em>
+        </p>
+        <p>{work.description}</p>
+      </div>
+    );
+  });
+  let skills = props.data.skills.map(function (skills) {
+    let className = "bar-expand " + skills.name.toLowerCase();
+    return (
+      <li key={skills.name}>
+        <span style={{ width: skills.level }} className={className}></span>
+        <em>{skills.name}</em>
+      </li>
+    );
+  });
+
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">{work}</div>
+      </div>
+
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span>Skills</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+
+          <div className="bars">
+            <ul className="skills">{skills}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
---
+++ src/utils/resumeData.js
@@ -0,0 +1,113 @@
+const resumeData = {
+  main: {
+    name: "Anuj Soni",
+    occupation: "Software Engineer",
+    description: "Software Engineer by Day, Avid Reader by Night.",
+    image: "anuj_profile_photo.jpeg",
+    bio: "My name is Anuj, I'm a Software Engineer.",
+    contactmessage: "Get in touch with me to receive further details.",
+    email: "anujsoni08@gmail.com",
+    phone: "+91 8847 312 070",
+    address: {
+      city: "Gurgaon",
+      state: "Haryana",
+      zip: "122004",
+    },
+    website: "http://www.anujsoni.netlify.app",
+    resumedownload: "http://anujsoni.netlify.app",
+    social: [
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/anujsoni333",
+        className: "fa fa-facebook",
+      },
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/anujsoni08/",
+        className: "fa fa-linkedin",
+      },
+      {
+        name: "instagram",
+        url: "https://www.instagram.com/asoni08/",
+        className: "fa fa-instagram",
+      },
+      {
+        name: "github",
+        url: "http://github.com/anujsoni08",
+        className: "fa fa-github",
+      },
+    ],
+  },
+  resume: {
+    skillmessage:
+      "Here you can create a short write-up of your skills to show off to employers",
+    education: [
+      {
+        school: "Lovely Professional Univesity",
+        degree: "B.Tech Computer Science",
+        graduated: "June 2018",
+        description: "Graduated with First Class",
+      },
+    ],
+    work: [
+      {
+        company: "Bigfoot Retail Solutions Pvt. Ltd.(Shiprocket)",
+        title: "Software Engineer",
+        years: "Oct 2020 - Present",
+        description: "Working as a Software Engineer Frontend",
+      },
+      {
+        company: "Sixty Four Squares Pvt. Ltd",
+        title: "Software Engineer",
+        years: "Jul 2020 - Oct 2020",
+        description: "Worked as a Software Engineer Frontend",
+      },
+      {
+        company: "Edfora Infotech",
+        title: "Software Engineer",
+        years: "May 2019 - Jun 2020",
+        description: "Worked as a Software Engineer (Full Stak)",
+      },
+      {
+        company: "Mountblue Technologies Pvt. Ltd.",
+        title: "Software Engineer",
+        years: "Feb 2019 - Jun 2020",
+        description: "Worked as a Software Engineer (Full Stak)",
+      },
+    ],
+    skills: [
+      {
+        name: "ReactJs",
+        level: "100%",
+      },
+      {
+        name: "CSS",
+        level: "95%",
+      },
+      {
+        name: "Git",
+        level: "60%",
+      },
+      {
+        name: "HTML5",
+        level: "80%",
+      },
+      {
+        name: "MongoDB",
+        level: "80%",
+      },
+    ],
+  },
+  portfolio: {
+    projects: [
+      {
+        title: "Counter Project",
+        category: "Counter Project built using React",
+        image: "counter-demo.jpeg",
+        url: "https://counter-demo.netlify.app/",
+      },
+    ],
+  },
+};
+
+export default resumeData;
